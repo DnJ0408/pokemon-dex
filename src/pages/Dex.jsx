013f93af83f95600d5e4aa1d41b753bc0923bfa3
DnJ0408 <oyj921205@gmail.com>
2025-02-04 11:00:14 +0900

    Add A DexPage

---
+++ src/pages/Dex.jsx
@@ -0,0 +1,15 @@
+import DashBoard from "../components/DashBoard";
+import PokemonList from "../components/PokemonList";
+
+const Dex = () => {
+
+
+    return (
+        <div>
+            <DashBoard />
+            <PokemonList />
+        </div>
+    )
+}
+
+export default Dex;
