49d3c668b382a6a533829762da8beeb4be509d6b
DnJ0408 <oyj921205@gmail.com>
2025-02-04 12:02:21 +0900

    ✨Update Choose a Pokemon

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,32 +1,30 @@
 import styled from "styled-components";
 import MOCK_DATA from "./data/MOCK_DATA"
 
-const PokemonList = () => {
-
-    const PokemonUl = styled.div`
-        list-style: none;
-        padding: 0;
-        margin: 0;
-        display: flex;
-        flex-wrap: wrap;
-    `
+const PokemonList = ({ addPokemon }) => {
 
     return (
         <div>
             <PokemonUl>
-                {MOCK_DATA.map((pokemon) => {
-                    return (
-                        <li key={pokemon.id}>
-                            <img src={pokemon.img_url} alt={pokemon.korean_name} />
-                            <h3>{pokemon.korean_name}</h3>
-                            <p>No.00{pokemon.id}</p>
-                            <button>ADD</button>
-                        </li>
-                    );
-                })}
+                {MOCK_DATA.map((pokemon) =>
+                    <li key={pokemon.id}>
+                        <img src={pokemon.img_url} alt={pokemon.korean_name} />
+                        <h3>{pokemon.korean_name}</h3>
+                        <p>No.00{pokemon.id}</p>
+                        <button onClick={() => addPokemon(pokemon)}>ADD</button>
+                    </li>
+                )}
             </PokemonUl>
         </div>
     )
 }
+
+const PokemonUl = styled.ul`
+list-style: none;
+padding: 0;
+margin: 0;
+display: flex;
+flex-wrap: wrap;
+`
 
 export default PokemonList;
