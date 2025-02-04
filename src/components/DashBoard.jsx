c922ea3a4816c4f574c65d654520ff55b3a839c0
DnJ0408 <oyj921205@gmail.com>
2025-02-04 12:01:41 +0900

    ✨Update Pokemon Grid

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,25 +1,43 @@
 import styled from "styled-components"
 
-const DashBoard = () => {
-
-    const DashBoardWrapper = styled.div`
-    background-color: #f9f9f9;
-    border-radius: 10px;
-    
-`
-
-    const Title = styled.div`
-        display: flex;
-        justify-content: center;
-    `
+const DashBoard = ({ selectedPokemons }) => {
 
     return (
         <div>
             <DashBoardWrapper>
                 <Title>My Own Pokémon</Title>
+                <PokemonGrid>
+                    {selectedPokemons.map((pokemon) => {
+                        return (
+                            <div key={pokemon.id}>
+                                <img src={pokemon.img_url} alt={pokemon.korean_name} width="50px" />
+                                <p>{pokemon.korean_name}</p>
+                                <p>No. 00{pokemon.id}</p>
+                                <button>삭제</button>
+                            </div>
+                        );
+                    })}
+                </PokemonGrid>
             </DashBoardWrapper>
         </div>
-    )
+    );
 }
+
+const DashBoardWrapper = styled.div`
+background-color: #f9f9f9;
+border-radius: 10px;
+
+`
+
+const Title = styled.h2`
+    display: flex;
+    justify-content: center;
+`
+
+const PokemonGrid = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+`
 
 export default DashBoard
