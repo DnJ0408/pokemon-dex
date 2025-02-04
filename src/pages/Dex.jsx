dc91c895867eab2c26fdf3be1ae020b7543b1f3f
DnJ0408 <oyj921205@gmail.com>
2025-02-04 12:06:02 +0900

    Create a useState for selectedPokemons and define the addPokemon function.

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import DashBoard from "../components/DashBoard";
 import PokemonList from "../components/PokemonList";
 
 const Dex = () => {
 
+    const [selectedPokemons, setSelectedPokemons] = useState([]);
 
+    const addPokemon = (pokemon) => {
+        if (selectedPokemons.length < 6) {
+            setSelectedPokemons((prev) => {
+                console.log(prev);
+                return [...prev, pokemon];
+            });
+        } else {
+            alert("최대 6마리까지만 선택할 수 있습니다.");
+        }
+    };
     return (
         <div>
-            <DashBoard />
-            <PokemonList />
+            <DashBoard selectedPokemons={selectedPokemons} />
+            <PokemonList addPokemon={addPokemon} />
         </div>
     )
 }
