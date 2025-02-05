020ec6244bbfab8703298d7f2bf76441efc3d1d9
DnJ0408 <oyj921205@gmail.com>
2025-02-05 22:08:23 +0900

    🎨 Style: Moved two styled-components outside of the Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,26 +4,6 @@ import styled from "styled-components";
 
 const Home = () => {
 
-    const Container = styled.div`
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    `
-
-    const LinkToDex = styled(Link)`
-        color: white;
-        text-decoration-line: none;
-        border: 1px solid red;
-        border-radius: 5px;
-        background-color: #ff0000;
-        padding: 10px;
-
-        &:hover {
-            background-color: #cc0000;
-        }
-        
-    `
-
     return (
         <Container>
             <div>
@@ -35,5 +15,26 @@ const Home = () => {
         </Container>
     )
 }
+
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+const LinkToDex = styled(Link)`
+color: white;
+text-decoration-line: none;
+border: 1px solid red;
+border-radius: 5px;
+background-color: #ff0000;
+padding: 10px;
+
+&:hover {
+    background-color: #cc0000;
+}
+
+`
 
 export default Home;
