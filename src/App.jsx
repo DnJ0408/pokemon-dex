452e8018a955cb520748985c5b7c2d89b0581443
DnJ0408 <oyj921205@gmail.com>
2025-02-05 22:17:41 +0900

    ✨ Update  React-toastify functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
 import Router from './shared/Router'
+import { ToastContainer, Bounce } from 'react-toastify'
 
 function App() {
 
   return (
     <>
       <Router />
+      <ToastContainer
+        position="top-right"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
     </>
   )
 }
