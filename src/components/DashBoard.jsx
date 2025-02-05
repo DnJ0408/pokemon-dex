c50dac4fbb411ffec3ff05fb0c6ab06519340a6e
DnJ0408 <oyj921205@gmail.com>
2025-02-05 22:20:17 +0900

    🩹 Modified Pokemon numbering using the PadStart method

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,25 +1,25 @@
 import styled from "styled-components"
 
-const DashBoard = ({ selectedPokemons, deletePokemon }) => {
+const DashBoard = ({ selectedPokemon, deletePokemon }) => {
 
     return (
         <div>
             <DashBoardWrapper>
                 <Title>My Own Pokémon</Title>
-                <PokemonGrid>
-                    {selectedPokemons.map((pokemon) => {
+                <SelectedPokemonCards>
+                    {selectedPokemon.map((pokemon) => {
                         return (
                             <DashBoardCardList key={pokemon.id}>
                                 <img src={pokemon.img_url} alt={pokemon.korean_name} width="50px" />
-                                <p>{pokemon.korean_name}</p>
-                                <p>No. 00{pokemon.id}</p>
+                                <h3>{pokemon.korean_name}</h3>
+                                <p>No.{pokemon.id.toString().padStart(3, "0")}</p>
                                 <DeleteButton onClick={() => deletePokemon(pokemon.id)}>
                                     Delete
                                 </DeleteButton>
                             </DashBoardCardList>
                         );
                     })}
-                </PokemonGrid>
+                </SelectedPokemonCards>
             </DashBoardWrapper>
         </div>
     );
@@ -36,10 +36,10 @@ const Title = styled.h1`
     justify-content: center;
 `
 
-const PokemonGrid = styled.div`
+const SelectedPokemonCards = styled.div`
     display: flex;
     justify-content: center;
-    gap: 100px;
+    gap: 50px;
 `
 
 const DashBoardCardList = styled.li`
@@ -60,8 +60,8 @@ const DashBoardCardList = styled.li`
     }
 
     img {
-        width: 100px;
-        height: 100px;
+        width: 140px;
+        height: 140px;
         object-fit: cover;
         margin-bottom: 10px;
     }
@@ -76,9 +76,10 @@ const DeleteButton = styled.button`
     cursor: pointer;
     font-weight: bold;
     margin-top: 8px;
+    transition: all 0.3s;
 
     &:hover {
-        background: #ff000073;
+        background: #ac0101;
     }
 `;
 
