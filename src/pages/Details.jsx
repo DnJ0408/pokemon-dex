b137b312150d48a756b5ff5280833b322749adde
DnJ0408 <oyj921205@gmail.com>
2025-02-05 22:13:22 +0900

    ✨ Added a detail page and loaded data using query strings

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,20 @@
+import { useLocation } from "react-router-dom";
+
 const Details = () => {
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+
+    const img = params.get("img");
+    const name = params.get("name");
+    const id = params.get("id");
+
     return (
-        <div>Details</div>
-    )
+        <div>
+            <img src={img} alt={name} />
+            <h2>{name}</h2>
+            <p>No.{id.padStart(3, "0")}</p>
+        </div>
+    );
 }
 
 export default Details
