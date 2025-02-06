7af6fb073c450ab34280cece7f33d35bea4ce82e
DnJ0408 <oyj921205@gmail.com>
2025-02-06 14:31:06 +0900

    🎨 Styled Global backgroundColor

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #f5deb3;
+    margin: 0;
+    padding: 0;
+  }
+`
+
+export default GlobalStyle;
