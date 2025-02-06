ad65933e83e71d2407d65e5dfba98bfee60e4d26
DnJ0408 <oyj921205@gmail.com>
2025-02-06 14:30:44 +0900

    🩹 Modified Image function name &  🎨 Styled title image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,43 @@
 import { Link } from "react-router-dom";
-import TitleImage from "../assets/logo.png"
+import TitleImageSrc from "../assets/pokemonLogo.png"
 import styled from "styled-components";
 
 const Home = () => {
 
     return (
         <Container>
-            <div>
-                <img src={TitleImage} style={{ width: "600px" }} />
-            </div>
-            <div>
-                <LinkToDex to="/dex">Create Your Pokédex</LinkToDex>
-            </div>
+            <TitleImage src={TitleImageSrc} alt="Pokémon Logo" />
+            <LinkToDex to="/dex">Create Your Pokédex</LinkToDex>
         </Container>
     )
 }
 
 
 const Container = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
 `
 
+const TitleImage = styled.img`
+    width: 600px;
+    max-width: 80%;
+    margin-bottom: 40px;
+`;
+
 const LinkToDex = styled(Link)`
-color: white;
-text-decoration-line: none;
-border: 1px solid red;
-border-radius: 5px;
-background-color: #ff0000;
-padding: 10px;
+    color: white;
+    text-decoration-line: none;
+    border: 1px solid red;
+    border-radius: 5px;
+    background-color: #ff0000;
+    padding: 10px;
 
-&:hover {
-    background-color: #cc0000;
-}
-
+    &:hover {
+        background-color: #cc0000;
+    }
 `
 
 export default Home;
