c05d34b3bfacd0fb4b0aff1adf819b8490b58575
DnJ0408 <oyj921205@gmail.com>
2025-02-07 15:00:25 +0900

    🚚 Move

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -5,16 +5,10 @@ import { toast } from "react-toastify";
 
 const Dex = () => {
 
+    //*--- 선택된 포켓몬 state ---*//
     const [selectedPokemon, setSelectedPokemon] = useState([]);
 
-    const deletePokemon = (id) => {
-        const deletedPokemon = selectedPokemon.filter((pokemon) => {
-            return pokemon.id !== id;
-        })
-        toast.success("포켓몬이 자연으로 돌아갔습니다.")
-        setSelectedPokemon(deletedPokemon);
-    }
-
+    //*--- 포켓몬 추가 함수 ---*//
     const addPokemon = (pokemon) => {
         if (selectedPokemon.some((selectedPokemon) => selectedPokemon.id === pokemon.id)) {
             toast.warning("한 마리 씩만 가져갈 수 있어요!");
@@ -23,19 +17,26 @@ const Dex = () => {
 
         if (selectedPokemon.length < 6) {
             setSelectedPokemon((prev) => {
-                toast.success(`${pokemon.korean_name}이(가) 추가 되었어요!`);
                 return [...prev, pokemon];
             });
+            toast.success(`${pokemon.korean_name}이(가) 추가 되었어요!`);
         } else {
             toast.warning("최대 6마리까지만 함께 할 수 있어요!");
         }
     };
+
+    //*--- 포켓몬 삭제 함수 ---*//
+    const deletePokemon = (id) => {
+        const deletedPokemon = selectedPokemon.filter((pokemon) => {
+            return pokemon.id !== id;
+        })
+        toast.success("포켓몬이 자연으로 돌아갔습니다.")
+        setSelectedPokemon(deletedPokemon);
+    }
+
     return (
         <div>
-            <DashBoard
-                selectedPokemon={selectedPokemon}
-                deletePokemon={deletePokemon}
-            />
+            <DashBoard selectedPokemon={selectedPokemon} deletePokemon={deletePokemon} />
             <PokemonList addPokemon={addPokemon} />
         </div>
     )
