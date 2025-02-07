91eb1ba9f914685dd9057ebc82dc868d226ecbb2
DnJ0408 <oyj921205@gmail.com>
2025-02-07 14:56:34 +0900

    🩹 Modify: Deleted useNavigate & changed props names

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,31 +1,25 @@
 import styled from "styled-components";
 import MOCK_DATA from "./data/MOCK_DATA"
-import { useNavigate } from "react-router-dom";
 import PokemonCard from "./PokemonCard";
 
 const PokemonList = ({ addPokemon }) => {
 
-    const navigate = useNavigate();
 
-    const handleCardClick = (pokemon) => {
-        navigate(`/details?img=${pokemon.img_url}&name=${pokemon.korean_name}&types=${pokemon.types}&description=${pokemon.description}`);
-        console.log(pokemon);
-    }
     return (
-        <PokemonUl>
+        <StPokemonUl>
             {MOCK_DATA.map((pokemon) =>
                 <PokemonCard
                     key={pokemon.id}
                     pokemon={pokemon}
-                    addPokemon={addPokemon}
-                    handleCardClick={handleCardClick}
+                    buttonText="ADD"
+                    clickButton={addPokemon}
                 />
             )}
-        </PokemonUl>
+        </StPokemonUl>
     )
 }
 
-const PokemonUl = styled.ul`
+const StPokemonUl = styled.ul`
     list-style: none;
     padding: 0;
     margin: 0;
