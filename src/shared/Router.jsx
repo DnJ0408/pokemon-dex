48b152c56f189d33714d275a31dcb57b20c46c69
DnJ0408 <oyj921205@gmail.com>
2025-02-09 22:03:14 +0900

    ✨ Update: Import PokemonContextProvider

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Dex from "../pages/Dex";
 import Details from "../pages/Details";
+import { PokemonContextProvider } from "../context/PokemonContext";
+
 
 const Router = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/dex' element={<Dex />} />
-                <Route path='/details' element={<Details />} />
-            </Routes>
-        </BrowserRouter>
+        <PokemonContextProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/dex' element={<Dex />} />
+                    <Route path='/details' element={<Details />} />
+                </Routes>
+            </BrowserRouter>
+        </PokemonContextProvider>
     )
 }
 
