aa5d804efd5bcc632f923157d32e01c3b047eb0f
DnJ0408 <oyj921205@gmail.com>
2025-02-11 18:49:47 +0900

    ✨ Updated Provider & Store prop

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import store from './redux/config/configStore.js'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+
+  //* <App> 컴포넌트 이하의 자식 컴포넌트 들은 configStore.js에 있는 state를 자유롭게 쓸 수 있다.
+  //* Provider의 속성으로는 store를 넣어준다. -> store={import한 저장소 이름}
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
